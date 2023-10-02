74dcf2ecd2d23641d1ce4176ff9f5f3ede921d95
eliseoBcn <125320599+eliseoBcn@users.noreply.github.com>
2023-10-02 08:38:00 +0200

    Autenticación  por JWT

--- src/main/java/cat/itacademy/barcelonactiva/escobarjulia/andres/s05/t02/n01/S05T2N01EscobarJuliaAndresApplication.java
+++ src/main/java/cat/itacademy/barcelonactiva/escobarjulia/andres/s05/t02/n01/S05T2N01EscobarJuliaAndresApplication.java
@@ -1,7 +1,24 @@
 package cat.itacademy.barcelonactiva.escobarjulia.andres.s05.t02.n01;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import cat.itacademy.barcelonactiva.escobarjulia.andres.s05.t02.n01.security.JWTAuthorizationFilter;
+
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @SpringBootApplication
 public class S05T2N01EscobarJuliaAndresApplication {
@@ -10,4 +27,27 @@ public class S05T2N01EscobarJuliaAndresApplication {
 		SpringApplication.run(S05T2N01EscobarJuliaAndresApplication.class, args);
 	}
 
+	@EnableWebSecurity
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	
+	class WebSecurityConfig  {
+
+	    @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	            // disabling csrf since we won't use form login
+	            .csrf().disable()
+ 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+	            // giving permission to every request for /login endpoint
+	            .authorizeRequests().antMatchers(HttpMethod.POST,"/user").permitAll()
+	            // for everything else, the user has to be authenticated
+	            .anyRequest().authenticated()
+	            // setting stateless session, because we choose to implement Rest API
+	            .and().sessionManagement()
+	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	        return http.build();
+	    }
+	}
 }
